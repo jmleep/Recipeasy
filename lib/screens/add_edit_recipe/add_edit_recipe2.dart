142e5838f5f3f9f0ca18ce887d6961e47c487403
Jordan <j.m.leep@gmail.com>
2021-01-04 18:01:15 -0500

    updated add edit view to show photos and use mini photo list display underneath

--- lib/screens/add_edit_recipe/add_edit_recipe2.dart
+++ lib/screens/add_edit_recipe/add_edit_recipe2.dart
@@ -7,7 +7,6 @@ import 'package:my_recipes/database/recipe_data_manager.dart';
 import 'package:my_recipes/model/recipe.dart';
 import 'package:my_recipes/model/recipe_photo.dart';
 import 'package:my_recipes/widgets/app_bar.dart';
-import 'package:my_recipes/widgets/buttons/button_primary_mini.dart';
 
 class AddEditRecipe2 extends StatefulWidget {
   final Recipe recipe;
@@ -22,6 +21,7 @@ class _AddEditRecipe2State extends State<AddEditRecipe2> {
   Future<List<RecipePhoto>> _recipeImages;
   List<RecipePhoto> _tempRecipePhotos = new List<RecipePhoto>();
   final _picker = ImagePicker();
+  var activePhoto = 0;
 
   void getRecipeImages() async {
     if (widget.recipe != null) {
@@ -42,23 +42,93 @@ class _AddEditRecipe2State extends State<AddEditRecipe2> {
     }
   }
 
+  Future saveTempListOfPhotos() async {
+    print('saved');
+  }
+
+  List<AppBarAction> getAppBarActions() {
+    return [
+      new AppBarAction(
+          Icon(
+            Icons.add_a_photo,
+            color: Theme.of(context).primaryColor,
+          ),
+          addImageToTempListOfPhotos),
+      new AppBarAction(
+          Icon(
+            Icons.check,
+            color: Theme.of(context).primaryColor,
+          ),
+          saveTempListOfPhotos)
+    ];
+  }
+
   Widget getPhotoList() {
     return ConstrainedBox(
-      constraints: new BoxConstraints(minHeight: 300, maxHeight: 300),
+      constraints: new BoxConstraints(minHeight: 100, maxHeight: 100),
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         shrinkWrap: true,
         itemCount: _tempRecipePhotos.length,
         itemBuilder: (context, index) {
           File image = new File(_tempRecipePhotos[index].value);
-          return Image.file(
-            image,
-            height: 300,
-            width: 150,
+          return GestureDetector(
+            onTap: () => setActivePhoto(index),
+            child: Image.file(
+              image,
+              height: 100,
+              width: 75,
+            ),
           );
         },
       ),
     );
+  }
+
+  swipeActivePhoto(DragEndDetails details) {
+    if (details.primaryVelocity < 0) {
+      setActivePhoto(activePhoto + 1);
+    } else {
+      setActivePhoto(activePhoto - 1);
+    }
+  }
+
+  setActivePhoto(int index) {
+    if (index < 0 || index >= _tempRecipePhotos.length) {
+      return;
+    }
+
+    setState(() {
+      activePhoto = index;
+    });
+  }
+
+  Widget getActivePhoto() {
+    var container;
+    if (_tempRecipePhotos.length > 0) {
+      container = Container(
+          color: Colors.red,
+          constraints: BoxConstraints.expand(),
+          child: Image.file(new File(_tempRecipePhotos[activePhoto].value)));
+    } else {
+      container = Container(
+        color: Colors.grey,
+        constraints: BoxConstraints.expand(),
+        child: Icon(
+          Icons.add_a_photo,
+          color: Colors.white,
+        ),
+      );
+    }
+
+    return Expanded(
+        child: GestureDetector(
+      onHorizontalDragEnd: (details) => swipeActivePhoto(details),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: container,
+      ),
+    ));
   }
 
   @override
@@ -72,26 +142,21 @@ class _AddEditRecipe2State extends State<AddEditRecipe2> {
     return Scaffold(
       appBar: RecipeAppBar(
         title: "Add Recipe",
+        actions: getAppBarActions(),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          getPhotoList(),
-          // Container(
-          //   width: 100,
-          //   height: 150,
-          //   color: Colors.grey,
-          //   child: Icon(
-          //     Icons.add_a_photo,
-          //     color: Colors.white,
-          //   ),
-          // ),
-
-          MiniPrimaryButton(
-              buttonText: 'Add Image',
-              onButtonPress: addImageToTempListOfPhotos),
-        ],
+      body: Container(
+        color: Colors.greenAccent,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              getActivePhoto(),
+              getPhotoList(),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -4,27 +4,51 @@ import 'package:google_fonts/google_fonts.dart';
 class RecipeAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final bool allowBack;
+  final List<AppBarAction> actions;
 
-  RecipeAppBar({this.title, this.allowBack = true});
+  RecipeAppBar({this.title, this.allowBack = true, this.actions});
+
+  List<Widget> getActions() {
+    List<Widget> actionButtons = [];
+
+    if (this.actions != null) {
+      this.actions.forEach((element) {
+        actionButtons.add(Padding(
+          padding: EdgeInsets.only(right: 20.0),
+          child: GestureDetector(onTap: element.callback, child: element.icon),
+        ));
+      });
+    }
+
+    return actionButtons;
+  }
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-        leading: allowBack ? BackButton(
-     color: Theme.of(context).primaryColor
-   ) : null, 
-        title: Text(title,
-            style: GoogleFonts.pacifico(
-              color: Theme.of(context).primaryColor,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              )),
-        elevation: 0.0,
-        centerTitle: true,
-        brightness: Brightness.dark,
-        backgroundColor: Theme.of(context).accentColor,);
+      leading:
+          allowBack ? BackButton(color: Theme.of(context).primaryColor) : null,
+      actions: <Widget>[...getActions()],
+      title: Text(title,
+          style: GoogleFonts.pacifico(
+            color: Theme.of(context).primaryColor,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          )),
+      elevation: 0.0,
+      centerTitle: true,
+      brightness: Brightness.dark,
+      backgroundColor: Theme.of(context).accentColor,
+    );
   }
 
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class AppBarAction {
+  Icon icon;
+  Function callback;
+
+  AppBarAction(this.icon, this.callback);
 }
