52d4f4a7a1ab85758685fa9135dbcbfdac2d22e5
Jordan Leeper <j.m.leep@gmail.com>
2022-11-20 14:21:42 -0600

    adds firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:my_recipes/screens/home/grid.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyRecipeApp());
 }
 
@@ -30,4 +37,3 @@ class MyRecipeApp extends StatelessWidget {
     );
   }
 }
-
